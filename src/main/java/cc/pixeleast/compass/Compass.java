89b59dd98ce77f01352a9a94439eb707e101f47b
Ican-291 <grass_20113@qq.com>
2024-08-15 13:16:26 +0800

    Initial commit

---
+++ src/main/java/cc/pixeleast/compass/Compass.java
@@ -0,0 +1,16 @@
+package cc.pixeleast.compass;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Compass extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
